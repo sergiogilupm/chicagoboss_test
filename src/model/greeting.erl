b7ee29392bfc26c3ea2680641417773b864498e8
Sergio Gil <sergio.gil.luque@gmail.com>
2016-08-08 17:10:12 -0500

    First commit

---
+++ src/model/greeting.erl
@@ -0,0 +1,8 @@
+-module(greeting, [Id, GreetingText]).
+-compile(export_all).
+
+validation_tests() ->
+	[{fun() -> length(GreetingText) > 0 end,
+		"Greeting must be non-empty!"},
+	 {fun() -> length(GreetingText) =< 140 end,
+		"Greeting must be tweetable"}].
